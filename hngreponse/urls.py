22a1ef2c213a2c6b8ec3848c8994bdf74fae5396
kesevan626 <87601398+kesevan626@users.noreply.github.com>
2022-10-31 14:50:29 -0700

    first commit

---
+++ hngreponse/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+from django.contrib import admin
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path('', include('response_app.urls')),
+    path("admin/", admin.site.urls),
+]
---
+++ response_app/views.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse 
+from rest_framework.decorators import api_view
+from django.shortcuts import render
+
+# Create your views here.
+response = {
+    'SlackUsername': 'Jacob Peter',
+    'backend': True,
+    'age': 24,
+    'bio': 'I am from ogun state,I am a backend developer'
+}
+
+@api_view(['Get'])
+def Home(request):
+    return JsonResponse(response)
