b5b5f71df388108a8b78745489492f5645075666
Jacob Peter <87601398+keseven626@users.noreply.github.com>
2022-11-04 20:04:55 -0700

    Added a new view

--- response_app/views.py
+++ response_app/views.py
@@ -13,3 +13,43 @@ response = {
 @api_view(['Get'])
 def Home(request):
     return JsonResponse(response)
+
+@api_view(['POST'])
+def performOperation(request):
+    if request.method == "POST":
+        operation_type = request.data.get('operation_type')
+        operation = str()
+        no_list = []
+        result = int()
+        x = request.data.get('x' or 'X')
+        y = request.data.get('y' or 'Y')
+
+        if '+' in operation_type or 'plus' in operation_type or 'add' in operation_type or 'addition' in operation_type:
+            operation = 'add'
+        elif '*' in operation_type or 'multiply' in operation_type or 'multiplication' in operation_type :
+            operation = 'multiply'
+        elif '-' in operation_type or 'substrate' in operation_type or 'minus' in operation_type:
+            operation = 'substrate'    
+
+        if x == "" or x == None  and y == "" or y == None:
+            arr = operation_type.split(' ')
+            for num in arr:
+                if num.isdigit():
+                    no_list.append(int(num))
+        else:
+            no_list.append(int(x))
+            no_list.append(int(y))
+
+        if operation == "add":
+            result = sum(no_list)
+        elif operation == "substrate":
+            result = no_list[0] - no_list[1]
+        elif operation == "multiply":
+            result = no_list[0] * no_list[1]
+
+        response = {
+                    'slackUsername': 'Keseven',
+                    'result':  result,
+                     "operation_type": operation
+                }
+    return JsonResponse(response, safe=False)
